7b261aaefcab68d07fdbb853f0b1384226aca950
sam-fsd <kabirusam037@gmail.com>
2025-11-11 10:01:52 +0300

    Start session at the beginning of AuthController for proper session handling

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../models/User.php';
 
+session_start();
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userModel = new User();
 
